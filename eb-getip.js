c11c333861437b1c64b416feb1eac0e7f755039f
Adriaan Pelzer <adriaan.pelzer@telegraph.co.uk>
2015-11-05 11:48:21 +0000

    Finish

--- eb-getip.js
+++ eb-getip.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 var A = require ( 'aws-sdk' );
-var EB = new A.ElasticBeanstalk ();
-var E = new A.EC2 ();
+var EB = new A.ElasticBeanstalk ( { region: 'eu-west-1' } );
+var E = new A.EC2 ( { region: 'eu-west-1' } );
 
 EB.describeEnvironmentResources ( {
     EnvironmentName: process.argv[2]
@@ -10,10 +10,14 @@ EB.describeEnvironmentResources ( {
     if ( error ) { return console.error ( error ); }
 
     E.describeInstances ( {
-        InstanceIds: result.EnvironmentResources.InstanceIds
+        InstanceIds: result.EnvironmentResources.Instances.map ( function ( instance ) {
+            return instance.Id;
+        } )
     }, function ( error, result ) {
         if ( error ) { return console.error ( error ); }
 
-        console.log ( result );
+        console.log ( result.Reservations.map ( function ( r ) {
+            return r.Instances[0].PublicIpAddress;
+        } )[ process.argv[3] || 0 ] );
     } );
 } );
